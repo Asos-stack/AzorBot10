ff87c55110998abc348a84487d0750c4f267b407
Asos-stack <76244844+Asos-stack@users.noreply.github.com>
2020-12-19 13:16:04 +0100

    asd
    asd

--- bot.js
+++ bot.js
@@ -1078,50 +1078,6 @@ Events.onError = function(text, text2, cache) {
 };
 
 //---------------------------------------------------------------------
-// Images
-// Contains functions for image management.
-//---------------------------------------------------------------------
-
-const JIMP = require("jimp");
-
-const Images = DBM.Images = {};
-
-Images.getImage = function(url) {
-	if(!url.startsWith("http")) url = Actions.getLocalFile(url);
-	return JIMP.read(url);
-};
-
-Images.getFont = function(url) {
-	return JIMP.loadFont(Actions.getLocalFile(url));
-};
-
-Images.createBuffer = function(image) {
-	return new Promise(function(resolve, reject) {
-		image.getBuffer(JIMP.MIME_PNG, function(err, buffer) {
-			if(err) {
-				reject(err);
-			} else {
-				resolve(buffer);
-			}
-		});
-	});
-};
-
-Images.drawImageOnImage = function(img1, img2, x, y) {
-	for(let i = 0; i < img2.bitmap.width; i++) {
-		for(let j = 0; j < img2.bitmap.height; j++) {
-			const pos = (i * (img2.bitmap.width * 4)) + (j * 4);
-			const pos2 = ((i + y) * (img1.bitmap.width * 4)) + ((j + x) * 4);
-			const target = img1.bitmap.data;
-			const source = img2.bitmap.data;
-			for(let k = 0; k < 4; k++) {
-				target[pos2 + k] = source[pos + k];
-			}
-		}
-	}
-};
-
-//---------------------------------------------------------------------
 // Files
 // Contains functions for file management.
 //---------------------------------------------------------------------
